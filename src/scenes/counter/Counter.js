f84a5e4ffd3028a8980c9146c089d5af9bab3133
Sylwia Brant <sylwia.brant@o2.pl>
2021-12-13 21:30:49 +0100

    Rebased with master

--- src/scenes/counter/Counter.js
+++ src/scenes/counter/Counter.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet, View } from 'react-native'
 import { colors } from 'theme'
